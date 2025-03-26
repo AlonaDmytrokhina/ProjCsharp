a672199594f8dd2c5ecd3fad51e3dff2906967b9
Alona Dmytrokhina <dmitrohina1708@gmail.com>
2025-03-26 10:19:54 +0200

    Added main interface and PersonViewModel class

--- AppPerson/Views/PersonView.xaml.cs
+++ AppPerson/Views/PersonView.xaml.cs
@@ -1,5 +1,8 @@
-﻿using AppPerson.Views;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -7,22 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace AppPerson
+namespace AppPerson.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for PersonView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class PersonView : Window
     {
-        public MainWindow()
+        public PersonView()
         {
             InitializeComponent();
-            var personWindow = new PersonView();
-            personWindow.Show();
-            this.Hide();
         }
     }
 }
